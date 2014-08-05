ccea4ef549ccbdef21f952eef4e031766a4069a8
lombax <iPodAlexei@gmail.com>
2014-08-05 04:33:23 -0400

    Initial Commit

---
+++ src/main/java/lombax5832/BL2_v2/BL2.java
@@ -0,0 +1,18 @@
+package lombax5832.BL2_v2;
+
+import lombax5832.BL2_v2.lib.Strings;
+import cpw.mods.fml.common.Mod;
+
+/**
+ * 
+ * Core class of BL2 v2!
+ * 
+ * @author lombax5832
+ */
+@Mod(modid = Strings.MOD_ID,name = Strings.MOD_NAME,version = Strings.MOD_VERSION)
+public class BL2 {
+	
+	@Mod.Instance(Strings.MOD_ID)
+	public static BL2 instance;
+	
+}
---
+++ src/main/java/lombax5832/BL2_v2/lib/Strings.java
@@ -0,0 +1,13 @@
+package lombax5832.BL2_v2.lib;
+
+/**
+ * Collection of constant strings
+ * 
+ * @author lombax5832
+ *
+ */
+public class Strings {
+	public static final String MOD_ID = "BL2";
+	public static final String MOD_NAME = "Borderlands 2";
+	public static final String MOD_VERSION = "0.1";
+}
