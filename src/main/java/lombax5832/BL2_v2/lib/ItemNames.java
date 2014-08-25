1919baafc50e59631e7e9cb39992ad706e8d4da9
lombax <iPodAlexei@gmail.com>
2014-08-25 17:19:44 -0400

    Revert "Working on new Models"
    This reverts commit eb3670192dcdfae51d5f6f0971fa95ca5882cf69.
    
    Conflicts:
    	src/main/java/lombax5832/BL2_v2/client/model/ModelCylinderTest.java
    	src/main/java/lombax5832/BL2_v2/common/item/ItemGun.java

--- src/main/java/lombax5832/BL2_v2/lib/ItemNames.java
+++
@@ -1,7 +0,0 @@
-package lombax5832.BL2_v2.lib;
-
-public class ItemNames {
-	public static final String BL2_PREFIX = "bl2_v2.";
-	
-	public static final String GUN_DEFAULT_NAME = BL2_PREFIX+"GunDefault";
-}
